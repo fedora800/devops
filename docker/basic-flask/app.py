852a3f523e98d6bcb26190fbdfda35e377281583
fedora800 <fedora800@example.com>
2024-08-22 19:55:12 +0000

    docker related variety of files changed

--- docker/basic-flask/app.py
+++ docker/basic-flask/app.py
@@ -1,17 +1,35 @@
 from flask import Flask,render_template
+import os
 import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except:
-        return render_template('error.html')
+  
+#  for data in os.environ:
+#    print(f"{data}={os.environ[data]}")
+
+  for name, value in sorted(os.environ.items()):
+     print("   " + name + "=" + value)
+
+  try:
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    return render_template('index.html', hostname=host_name, ip=host_ip)
+  except:
+    return render_template('error.html')
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+
+
+import numpy as np
+
+my_dict = {'yash': 2, 'rajnish': 9,
+        'sanjeev': 15, 'chanda': 10, 'suraj': 32}
+
+
+print("-----sorting by key-----")
